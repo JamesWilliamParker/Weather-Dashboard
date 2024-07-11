eefc72814ea348ff4cb10b7dc2d5c0e309f6e97d
James Parker <jamesparker.us@gmail.com>
2024-07-10 20:36:51 -0400

    function rendercurrent weather to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,12 +24,25 @@ function getData() {
 function renderCurrentWeather(city,weather){
     const temp = weather.list[0].main.temp;
     const wind = weather.list[0].wind.speed;
-    const humidity = weather.list[0].main.humidity;
+    const humid = weather.list[0].main.humidity;
     const icon = weather.list[0].weather[0].icon;
-    const iconURL = `https://openweathermap.org/img/wn/${icon}.png`;
+    const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
     
+    const container = document.getElementById('container');
+    const tempH1 = document.createElement('h1');
+    const windH1 = document.createElement('h1');
+    const humidH1 = document.createElement('h1');
+    const iconImg = document.createElement('img');
+    // console.log(iconURL);
     // console.log(iconURL);
     // console.log(weather.list[0].wind.speed);
+
+    tempH1.textContent = temp;
+    windH1.textContent = wind;
+    humidH1.textContent = humid;
+    iconImg.setAttribute ('src', iconUrl);
+
+    container.append(tempH1, windH1, humidH1, iconImg);
 }
 
 searchButton.addEventListener('click', getData);
